b902c22360017a0cd931b498ded8fc98b1b1a1bd
Tracey26 <tordonez@brooklynsteamcenter.org>
2024-12-12 08:47:06 -0500

    Created working search bar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql
 from dynaconf import Dynaconf
 
@@ -26,12 +26,21 @@ def index():
 
 @app.route("/browse")
 def product_browse():
+    query = request.args.get('query')
+
     conn = connect_db()
 
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM `Product` ;")
+    if query is None:
+        cursor.execute("SELECT * FROM `Product`;")
+    else:
+        cursor.execute(f"SELECT * FROM `Product` WHERE `product_name` LIKE '%{query}%' ;")
 
     results = cursor.fetchall()
 
     return render_template("browse.html.jinja", products = results)
+
+    cursor.close()
+    conn.close()
+
