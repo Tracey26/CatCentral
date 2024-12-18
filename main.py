fb436cf0f6e4f751d82cd134d905e7735bcee92d
Tracey26 <tordonez@brooklynsteamcenter.org>
2024-12-18 10:51:52 -0500

    Made sign in and sign up functional

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
 from flask import Flask, render_template, request, redirect, flash
-import pymysql
-from dynaconf import Dynaconf
+import flask_login
 
-app = Flask(__name__)
+import pymysql
+
+from dynaconf import Dynaconf
 
 conf = Dynaconf(
     settings_file = ("settings.toml")
 )
 
+app = Flask(__name__)
 app.secret_key = conf.secret_key
+
+login_manager= flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, id, username, email, first_name, last_name):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_id(self):
+        return str(self.id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `id` = {user_id};")
+
+    result = cursor.fetchone()
+
+    cursor.close
+    conn.close
+
+    if result is not None:
+        return User (result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+
 
 def connect_db():
     conn = pymysql.connect(
@@ -102,3 +138,36 @@ def signup():
             conn.close()
         
     return render_template("signup.html.jinja")
+
+@app.route("/signin", methods = ["POST", "GET"])
+def sign_in():
+    if request.method == "POST":
+        username = request.form['username'].strip()
+        password = request.form['password']
+
+        conn = connect_db()
+        cursor = conn.cursor()
+
+        cursor.execute(f"SELECT * FROM `Customer` WHERE `username` = '{username}';")
+
+        result = cursor.fetchone()
+
+        if result is None:
+            flash("Username/Password is incorrect")
+        elif password != result["password"]:
+            flash("Your username/password is incorrect")
+        else:
+            user = User (result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+
+            flask_login.login_user(user)
+
+            return redirect('/')
+
+
+        return render_template("signin.html.jinja")
+    
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
